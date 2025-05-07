aca6bf2de1cd08754c80c0e229aba00b3d6ef80a
augustoams01 <augustoams01@gmail.com>
2025-05-07 14:28:55 -0300

    nav-finalization

---
+++ resources/js/Pages/Posts.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainNavLayout from '@/Layouts/MainNavLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Posts" />
+
+    <MainNavLayout>
+        <h1>Posts</h1>
+    </MainNavLayout>
+
+</template>
